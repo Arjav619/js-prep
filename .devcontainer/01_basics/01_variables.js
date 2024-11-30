d6e884b859e380276f83a7954d22815d40ea84be
Arjav619 <68985926+Arjav619@users.noreply.github.com>
2024-11-30 19:40:46 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+
+const accountId=22
+var accountCity="delhi"
+var accountPass="2234"
+let accountEmail="kkj@gmail.com"
+accountpass="334"
+accountState="nnr"
+// var use na karna block scope functional scope k chakkar mein
+accountCity="jaipr"
+accountEmail="syys@gmail.com"
+//accountId=1
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";// naya version of js hoga
+//alert(3+3) //we are using nodejs not browser
+console.log(3+3)
+ console.log("nini")
+ let name="sisir"
+ let age=18;
+ let isLogged=false;
+
+ //number
+ //bigint
+ //boolean
+ //string=
+ //null = standalone vaule
+ //undefined-- i.e value is not assigned
+ //symbol= uniqeness
+ //object
+ console.log(typeof"sisir");
+ console.log(typeof null);
+ 
+ console.log(typeof undefined);
+ 
+ 
+ 
