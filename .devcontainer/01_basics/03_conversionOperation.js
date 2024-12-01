2bd15c51658d462c67f633ed7fb82a6d2ab62727
Arjav619 <68985926+Arjav619@users.noreply.github.com>
2024-12-01 10:42:19 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score=33;
+console.log(typeof score); // or as a method
+console.log(typeof(score));
+
+let valueInNum=Number(score)
+console.log(typeof score);
+let score1="33aba"
+console.log(typeof score1);
+//33--33
+//33abc--Nan
+//true 1, 0-false
+let isLoggedIn=""
+let booleanIsLoggedIN=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIN);
+//conversion into string aram se hogaya
+let SomeNum=33
+let StrNum=String(SomeNum)
+console.log(typeof StrNum);
